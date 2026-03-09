983415bbd6716e08bdacd96f75a62516eac4dfef
35IQ <161868577+35IQ@users.noreply.github.com>
2026-03-10 00:10:27 +0100

    Add files via upload

---
+++ src/register_types.h
@@ -0,0 +1,13 @@
+#ifndef REGISTER_TYPES_H
+#define REGISTER_TYPES_H
+
+
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_AudioEffectConvolutionReverb_module( ModuleInitializationLevel p_level );
+void uninitialize_AudioEffectConvolutionReverb_module( ModuleInitializationLevel p_level );
+
+#endif // !REGISTER_TYPES_H
